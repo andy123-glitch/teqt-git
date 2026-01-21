64ecc00c6acc28b5f62ee72d802e0af7cf0d3095
andy123-glitch <andy.azerot@gmail.com>
2026-01-21 16:16:26 +0100

    add image and configuration prettier

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 'warn',
+      'react/react-in-jsx-scope': 'off',
     },
   },
-])
+]);
