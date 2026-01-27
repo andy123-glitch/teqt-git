2f8ceef8528ce7f16565abfac39de3f40d2d4e74
andy123-glitch <andy.azerot@gmail.com>
2026-01-27 16:18:51 +0100

    add carousel component; redirect to 404 page if no id find

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,32 +1,40 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import DropdownButton from '../components/DropdownButton';
 import Profile from '../components/Profile';
 import Stars from '../components/Stars';
 import Footer from '../layout/Footer';
 import Header from '../layout/Header';
 import datas from '../../data/data.json';
+import Carousel from '../components/Carousel';
+import { useEffect } from 'react';
 
 function Logement() {
   const { id } = useParams();
+  const nav = useNavigate();
+  const find = datas.find((data) => data.id === id) ?? null;
+  useEffect(() => {
+    if (find === null) {
+      nav('/404');
+      return null;
+    }
+  }, [nav, find]);
 
-  const find = datas.find((data) => data.id === id);
-  if (find === undefined) window.location = '/';
-
+  if (find === null) return null
   const {
     title,
-    pictures,
     host,
     description,
     rating,
     location,
     equipments,
     tags,
+    pictures,
   } = find;
   return (
     <>
       <div className="main-content logement-page">
         <Header />
-        <img className="cover" src={pictures[0]} />
+        <Carousel pictures={pictures} />
         <div className="first-row">
           <section className="headings">
             <h1>{title}</h1>
@@ -36,14 +44,16 @@ function Logement() {
         </div>
         <div className="logement">
           <div className="tags">
-            {tags.map((tag,index) => (
-              <div className="utag" key={`${index}-${tag}`}>{tag}</div>
+            {tags.map((tag, index) => (
+              <div className="utag" key={`${index}-${tag}`}>
+                {tag}
+              </div>
             ))}
           </div>
           <Stars number={rating} />
           <DropdownButton title="Description">{description}</DropdownButton>
           <DropdownButton title="Équipements">
-            {equipments.map((equipment,index) => (
+            {equipments.map((equipment, index) => (
               <div key={`${index}-${equipment}`}>- {equipment}</div>
             ))}
           </DropdownButton>
